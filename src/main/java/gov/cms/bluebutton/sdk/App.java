1d6f4add0bfe66ead68ae42500a1383359b378dc
James Fuqian <jfuqian@ellumen.com>
2022-02-03 17:30:51 -0800

    just leave a place holder class, to be removed in impl phase.

--- src/main/java/gov/cms/bluebutton/sdk/App.java
+++ src/main/java/gov/cms/bluebutton/sdk/App.java
@@ -1,11 +1,8 @@
 package gov.cms.bluebutton.sdk;
 
 /**
- * Hello world!
+ * Place holder class, to be removed
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 }
